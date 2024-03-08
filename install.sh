37a0a66b28895cc9196285d8f5d851b2585680a9
nicococococ <64148276+nicococococ@users.noreply.github.com>
2024-03-09 00:44:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 stty erase ^H
+echo '#!/usr/bin/env bash'>>/smhauto.sh
+echo 'screen -dmS smhauto'>>/smhauto.sh
+echo 'screen -x -S smhauto -p 0 -X stuff $'cd /linux''>>/smhauto.sh
+echo 'screen -x -S smhauto -p 0 -X stuff $'\n''>>/smhauto.sh
+echo 'screen -x -S smhauto -p 0 -X stuff "h9-miner-spacemesh-linux-amd64 -gpuServer -license yes"'>>/smhauto.sh
+echo 'screen -x -S smhauto -p 0 -X stuff $'\n''>>/smhauto.sh
