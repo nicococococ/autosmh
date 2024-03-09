6564c4968d27dd30ef472340b953f463c46026cc
nicococococ <64148276+nicococococ@users.noreply.github.com>
2024-03-09 11:23:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ echo 'screen -x -S smh -p 0 -X stuff $'cd /linux''>>/$PlotServerPath/smhauto.sh
 echo 'screen -x -S smh -p 0 -X stuff $'\n''>>/$PlotServerPath/smhauto.sh
 echo 'screen -x -S smh -p 0 -X stuff "h9-miner-spacemesh-linux-amd64 -gpuServer -license yes"'>>/$PlotServerPath/smhauto.sh
 echo 'screen -x -S smh -p 0 -X stuff $'\n''>>/$PlotServerPath/smhauto.sh
-
 #添加到自启动
 sed -i '$i /$PlotServerPath/smhauto.sh' /home/user/xinit.user.sh
-
 echo -e "重启后，输入Screen -R smh 查看算力界面"
