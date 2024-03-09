846e9f4c90181cfb32c55349c0a984386646c4ef
nicococococ <64148276+nicococococ@users.noreply.github.com>
2024-03-09 11:11:26 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 stty erase ^H
-echo '#!/usr/bin/env bash'>>/smhauto.sh
-echo 'screen -dmS smhauto'>>/smhauto.sh
-echo 'screen -x -S smhauto -p 0 -X stuff $'cd /linux''>>/smhauto.sh
-echo 'screen -x -S smhauto -p 0 -X stuff $'\n''>>/smhauto.sh
-echo 'screen -x -S smhauto -p 0 -X stuff "h9-miner-spacemesh-linux-amd64 -gpuServer -license yes"'>>/smhauto.sh
-echo 'screen -x -S smhauto -p 0 -X stuff $'\n''>>/smhauto.sh
+echo && echo -n -e "请输入h9-miner-spacemesh-linux-amd64的所在路径，示例：/linux: " && read PlotServerPath
+#开启plot
+sed -i 's/disablePlot: true/disablePlot: false/g' /$PlotServerPath/config.yaml
+#生成bash
+[ -d /$PlotServerPath/smhauto.sh ] && rm -rf /$PlotServerPath/smhauto.sh
+touch /$PlotServerPath/smhauto.sh
+echo '#!/usr/bin/env bash'>>/$PlotServerPath/smhauto.sh
+echo 'screen -dmS smh'>>/$PlotServerPath/smhauto.sh
+echo 'screen -x -S smh -p 0 -X stuff $'cd /linux''>>/$PlotServerPath/smhauto.sh
+echo 'screen -x -S smh -p 0 -X stuff $'\n''>>/$PlotServerPath/smhauto.sh
+echo 'screen -x -S smh -p 0 -X stuff "h9-miner-spacemesh-linux-amd64 -gpuServer -license yes"'>>/$PlotServerPath/smhauto.sh
+echo 'screen -x -S smh -p 0 -X stuff $'\n''>>/$PlotServerPath/smhauto.sh
+
+#添加到自启动
+sed -i '$i /$PlotServerPath/smhauto.sh' /home/user/xinit.user.sh
+
+echo -e "重启后，输入Screen -R smh 查看算力界面"
